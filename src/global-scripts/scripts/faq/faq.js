62e4dd6aa22b3a32fd5720d6f7da9aaf032b7a2e
karoljanowski <karol.jj@icloud.com>
2023-03-09 13:53:03 +0100

    add features

---
+++ src/global-scripts/scripts/faq/faq.js
@@ -0,0 +1,37 @@
+/// Sample js file for demo purposes
+/// Remember to always export your functions
+/// so that they can be used in other files
+
+export default function faq(element) {
+  element.forEach(event => {
+    event.addEventListener('click', function () {
+
+      if (event.classList.contains('faq__active')) {
+        close(event)
+        window.addEventListener('resize', function () {
+          setHeight(event)
+        })
+
+      } else {
+        element.forEach(item => {
+          if (item.classList.contains('faq__active')) {
+            close(item)
+          }
+        })
+        event.classList.add("faq__active");
+        event.children[0].children[1].src = './public/images/minus.png'
+        setHeight(event)
+      }
+    })
+  })
+}
+
+function setHeight(element) {
+  element.children[1].style.height = element.children[1].scrollHeight + 'px'
+}
+
+function close(element) {
+  element.classList.remove("faq__active");
+  element.children[1].style.height = 0
+  element.children[0].children[1].src = './public/images/plus.png'
+}
---
+++ src/pages/home-page/scripts/home-page.js
@@ -0,0 +1,8 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import '../styles/home-page.scss'
+
+import faq from '../../../global-scripts/scripts/faq/faq.js'
+
+faq(document.querySelectorAll('.faq__question'))
